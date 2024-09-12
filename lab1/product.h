aa9203b720fc85f75d4b38a97e4e8681a031abef
Waaanther <nnelkane@gmail.com>
2024-09-12 15:37:54 +0300

    fix memory

--- lab1/product.h
+++ lab1/product.h
@@ -4,39 +4,37 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 class Product{
 private:
-    string name;
+    std::string name;
     double retailPrice;
     double wholesalePrice;
     int quantity;
 public:
-    Product(const string& n, double rPrice, double wPrice, int q)
+    Product(const std::string& n, double rPrice, double wPrice, int q)
         : name(n), retailPrice(rPrice), wholesalePrice(wPrice), quantity(q) {}
     
-    void setName(const string& n) { name = n; }
+    void setName(const std::string& n) { name = n; }
     void setRetailPrice(double rPrice) { retailPrice = rPrice; }
     void setWholesalePrice(double wPrice) {wholesalePrice = wPrice; }
     void setQuantity(int q) { quantity = q; }
 
-    string getName() const {return name;}
+    std::string getName() const {return name;}
     double getRetailPrice() const {return retailPrice;}
     double getWholesalePrice(bool isAdmin) const {
         if (isAdmin) {
             return wholesalePrice;
         }else{
-            cout << "Доступ запрещен: только администратор может просматривать оптовую цену!" << endl;
+            std::cout << "Доступ запрещен: только администратор может просматривать оптовую цену!" << std::endl;
             return -1;
         }
     }
     int getQuantity() const { return quantity; } 
 
     void displayProduct(bool isAdmin) const {
-        cout << "Название: " << name << ", Розничная цена: " << retailPrice << ", Количество: " << quantity << endl;
+        std::cout << "Название: " << name << ", Розничная цена: " << retailPrice << ", Количество: " << quantity << std::endl;
         if (isAdmin) {
-            cout << "Оптовая цена: " << wholesalePrice << endl;
+            std::cout << "Оптовая цена: " << wholesalePrice << std::endl;
         }
     }
 };
