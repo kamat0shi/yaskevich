8329152ce814903f5dbe14f43ff9f50601bba646
Waaanther <nnelkane@gmail.com>
2024-10-25 00:45:28 +0300

    fix sonar

--- Headers/Mainwindow.hpp
+++ Headers/Mainwindow.hpp
@@ -11,7 +11,7 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    exeplit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
