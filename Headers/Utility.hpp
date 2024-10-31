3fa19af2dc684c0ea298d4ef0dab7776b3894549
Waaanther <nnelkane@gmail.com>
2024-10-31 05:58:13 +0300

    +lab5fix2

--- Headers/Utility.hpp
+++ Headers/Utility.hpp
@@ -10,20 +10,25 @@
 template <typename Container, typename Getter>
 std::optional<std::string> printInfoByName(const Container& container, const std::string& name, Getter getName, bool isAdmin = false) {
     std::stringstream ss;
+
     for (const auto& item : container) {
-        if (getName(*item) == name) {
-            if constexpr (std::is_same_v<typename Container::value_type::element_type, Product>) {
-                ss << "Название: " << item->getName() << ", Розничная цена: " << item->getRetailPrice();
-                if (isAdmin) {
-                    ss << ", Оптовая цена: " << item->getWholesalePrice(isAdmin);
-                }
-                ss << ", Количество: " << item->getQuantity() << "\n";
-            } else {
-                ss << *item << "\n";
-            }
-            return ss.str();
+        if (getName(*item) != name) {
+            continue;
         }
+
+        if constexpr (std::is_same_v<typename Container::value_type::element_type, Product>) {
+            ss << "Название: " << item->getName() 
+               << ", Розничная цена: " << item->getRetailPrice();
+
+            if (isAdmin) {
+                ss << ", Оптовая цена: " << item->getWholesalePrice(isAdmin);
+            }
+        } else {
+            ss << *item;
+        }
+        return ss.str();
     }
+
     return std::nullopt;
 }
 
